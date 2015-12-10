3811902c4304d280e808c7a03e599cb7e4e41c11
Anson <anson32330200@gmail.com>
2015-12-10 12:05:45 +0800

    Use ngshow directive with button to hide and show details

--- conFusion_ng/app/scripts/app.js
+++ conFusion_ng/app/scripts/app.js
@@ -1,13 +1,14 @@
 'use strict';
 
 var app = angular.module('confusionApp',[]);
-app.controller('menuController', function() {
-    this.tab = 1;
-    this.filtText = "";
+app.controller('MenuController', ['$scope', function($scope) {
+    $scope.tab = 1;
+    $scope.filtText = "";
+    $scope.showDetails = false;
     // Since the first tab is selected as default,
     // the filtText should not filter out any item from the menu.
     // Hence filtText is set to the empty string.
-    var dishes=[
+    $scope.dishes=[
         {
            name:'Uthapizza',
            image: 'images/uthapizza.png',
@@ -45,23 +46,26 @@ app.controller('menuController', function() {
            comment: ''
         }
     ];      // end of dishes initialization
-    this.dishes = dishes;
-    this.select = function(setTab) {
-        this.tab = setTab;
+
+    $scope.select = function(setTab) {
+        $scope.tab = setTab;
         if (setTab === 2)   {
-            this.filtText = "appetizer";
+            $scope.filtText = "appetizer";
         }
         else if (setTab === 3)  {
-            this.filtText = "mains";
+            $scope.filtText = "mains";
         }
         else if (setTab === 4)  {
-            this.filtText = "dessert";
+            $scope.filtText = "dessert";
         }
         else {
-            this.filtText = "";
-        }            
+            $scope.filtText = "";
+        }
     };
-    this.isSelected = function(checkTab)    {
-        return (this.tab === checkTab);
+    $scope.isSelected = function(checkTab)    {
+        return ($scope.tab === checkTab);
     };
-}); // end of function in controller
+    $scope.toggleDetails = function()   {
+        $scope.showDetails = !$scope.showDetails;
+    };
+}]); // end of function in controller
