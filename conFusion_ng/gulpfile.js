2f179d2e75f062ffd515248d233eb9f8f4802627
Anson <anson32330200@gmail.com>
2015-12-23 17:17:40 +0800

    Update brower-sync and usemin

--- conFusion_ng/gulpfile.js
+++ conFusion_ng/gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('jshint', function() {
 });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('app/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
@@ -66,8 +66,9 @@ gulp.task('browser-sync', ['default'], function () {
     browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
-      }
+         index: "index.html"
+     },
+     reloadDelay: 1000
     });
         // Watch any files in dist/, reload on change
     gulp.watch(['dist/**']).on('change', browserSync.reload);
