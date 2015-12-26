4d19f9aff06acbc1b85db73b5be329b41bcc632b
Anson <anson32330200@gmail.com>
2015-12-26 14:23:23 +0800

    Modify to also pipe files to json-server/public

--- conFusion_ng/gulpfile.js
+++ conFusion_ng/gulpfile.js
@@ -28,28 +28,33 @@ gulp.task('usemin',['jshint'], function () {
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
       }))
-      .pipe(gulp.dest('dist/'));
+      .pipe(gulp.dest('dist/'))
+      .pipe(gulp.dest('json-server/public/'));
 });
 
 // Views
 gulp.task('copyviews', function() {
 return gulp.src('app/views/*')
-    .pipe(gulp.dest('dist/views'));
+    .pipe(gulp.dest('dist/views'))
+    .pipe(gulp.dest('json-server/public/views'));
 });
 
 // Images
 gulp.task('imagemin', function() {
-  return del(['dist/images']), gulp.src('app/images/**/*')
+  return del(['dist/images']), del(['json-server/public/images']), gulp.src('app/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/images'))
+    .pipe(gulp.dest('json-server/public/images'))
     .pipe(notify({ message: 'Images task complete' }));
 });
 
 gulp.task('copyfonts', ['clean'], function() {
    gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
+   .pipe(gulp.dest('./dist/fonts'))
+   .pipe(gulp.dest('json-server/public/fonts'));
    gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-   .pipe(gulp.dest('./dist/fonts'));
+   .pipe(gulp.dest('./dist/fonts'))
+   .pipe(gulp.dest('json-server/public/fonts'));
 });
 
 // Watch
@@ -82,7 +87,13 @@ gulp.task('browser-sync', ['default'], function () {
 
 // Clean
 gulp.task('clean', function() {
+    del(['json-server/public']);
     return del(['dist']);
+});
+
+// Clear task, use when images do not get copied to destination folder
+gulp.task('clear', function(done){
+  return cache.clearAll(done);
 });
 
 // Default task
