ae8a18e430168e84a34f53b0d141d0b88196076b
Anson <anson32330200@gmail.com>
2016-01-28 10:33:43 +0800

    Modify scaffolded application to develop application

--- Ionic/conFusion/www/js/app.js
+++ Ionic/conFusion/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('conFusion', ['ionic', 'conFusion.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,48 +26,58 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   $stateProvider
 
     .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
+        url: '/app',
+        abstract: true,
+        templateUrl: 'templates/sidebar.html',
+        controller: 'AppCtrl'
     })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+    .state('app.home', {
+        url: '/home',
+        views: {
+          'mainContent': {
+            templateUrl: 'templates/home.html'
+          }
+        }
+    })
+
+    .state('app.aboutus', {
+        url: '/aboutus',
+        views: {
+        'mainContent': {
+          templateUrl: 'templates/aboutus.html'
+        }
+        }
+    })
+
+    .state('app.contactus', {
+        url: '/contactus',
+        views: {
+        'mainContent': {
+          templateUrl: 'templates/contactus.html'
+        }
+        }
+    })
+
+    .state('app.menu', {
+        url: '/menu',
+        views: {
+        'mainContent': {
+          templateUrl: 'templates/menu.html',
+          controller: ''
+        }
+        }
+    })
+
+  .state('app.dishdetails', {
+        url: '/menu/:id',
+        views: {
+            'mainContent': {
+            templateUrl: 'templates/dishdetail.html',
+            controller: ''
+            }
+        }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/home');
 });
