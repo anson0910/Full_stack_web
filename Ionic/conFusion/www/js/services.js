c12034a4dc1fed8fe16da55ede971cc678137a52
Anson <anson32330200@gmail.com>
2016-01-29 15:33:44 +0800

    Add services file

--- Ionic/conFusion/www/js/services.js
+++ Ionic/conFusion/www/js/services.js
@@ -6,12 +6,12 @@ angular.module('conFusion.services', ['ngResource'])
 
             var promotions = [
                 {
-                          _id:0,
-                          name:'Weekend Grand Buffet',
-                          image: 'images/buffet.png',
-                          label:'New',
-                          price:'19.99',
-                          description:'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person ',
+                    _id:0,
+                    name:'Weekend Grand Buffet',
+                    image: 'images/buffet.png',
+                    label:'New',
+                    price:'19.99',
+                    description:'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person ',
                 }
 
             ];
