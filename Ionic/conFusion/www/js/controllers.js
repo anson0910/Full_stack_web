c34ddf93581b46f5273bd72e0840c6584e0d2f16
Anson <anson32330200@gmail.com>
2016-01-31 11:45:57 +0800

    Finish basic aboutus and contactus pages

--- Ionic/conFusion/www/js/controllers.js
+++ Ionic/conFusion/www/js/controllers.js
@@ -181,8 +181,9 @@ angular.module('conFusion.controllers', [])
 
             }])
 
-        .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
+        .controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
 
+                $scope.baseURL = baseURL;
                 $scope.leaders = corporateFactory.query();
                 console.log($scope.leaders);
 
