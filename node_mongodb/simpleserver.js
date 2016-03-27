6d57e02723546391fbb3991f24f0726d4c509651
Anson <anson32330200@gmail.com>
2016-03-27 15:50:11 +0800

    Simple Node-MongoDB Application

---
+++ node_mongodb/simpleserver.js
@@ -0,0 +1,27 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://localhost:27017/conFusion';
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, db)  {
+  assert.equal(err, null);
+  console.log("Connected correctly to the server");
+
+  var collection = db.collection("dishes");
+  collection.insertOne({name: "Uthapizza", description: "test"}, function(err, result)  {
+    assert.equal(err, null);
+    console.log("After insert");
+    console.log(result.ops);
+
+    collection.find({}).toArray(function(err, docs) {
+      assert.equal(err, null);
+      console.log("Found:");
+      console.log(docs);
+      db.dropCollection("dishes", function(err, result) {
+        assert.equal(err, null);
+        db.close();
+      });
+    });
+  });
+});
