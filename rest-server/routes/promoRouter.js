a9f59b12cf23fd7fad98ef87638c6c4397a75dae
Anson <anson32330200@gmail.com>
2016-04-01 23:05:26 +0800

    Develop full-fledged REST API server with Express, Mongo and Mongoose

--- rest-server/routes/promoRouter.js
+++ rest-server/routes/promoRouter.js
@@ -1,41 +1,64 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var promoRouter = express.Router();
+var Promotions = require('../models/promotions');
 
-promoRouter.use(bodyParser.json());
+var promotionRouter = express.Router();
+promotionRouter.use(bodyParser.json());
 
-promoRouter.route('/')
-  .all(function(req, res, next) {
-    res.status(200);
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-  })
-
+promotionRouter.route('/')
   .get(function(req, res, next) {
-    res.end('Will sent all promotions to you!');
+    Promotions.find({}, function(err, promotions) {
+      if (err)  throw err;
+      // Return promotions in JSON format
+      res.json(promotions);
+    });
   })
 
   .post(function(req, res, next) {
-    res.end('Will add the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+    Promotions.create(req.body, function(err, promotion) {
+      if (err)  throw err;
+      console.log("Promotion created!");
+      var id = promotion._id;
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.end("Added the promotion with id: " + id);
+    });
   })
 
   .delete(function(req, res, next) {
-    res.end('Deleting all promotions');
+    Promotions.remove({}, function(err, resp) {
+      if (err)  throw err;
+      res.json(resp);
+    });
   });
 
-promoRouter.route('/:promoId')
+promotionRouter.route('/:promotionId')
   .get(function(req, res, next) {
-    res.end('Will sent details of the promotion: ' + req.params.promoId + ' to you!');
+    Promotions.findById(req.params.promotionId, function(err, promotion) {
+      if (err)  throw err;
+      res.json(promotion);
+    });
   })
 
   .put(function(req, res, next) {
-    res.write('Updating the promotion: ' + req.params.promoId + '\n');
-    res.end('Will update the promotion: ' + req.body.name + ' with details: ' + req.body.description);
+    Promotions.findByIdAndUpdate(req.params.promotionId, {
+      $set: req.body
+    }, {
+      new: true
+    }, function(err, promotion) {
+
+      res.json(promotion);
+    });
   })
 
   .delete(function(req, res, next) {
-    res.end('Deleting promotion: ' + req.params.promoId);
+    Promotions.findByIdAndRemove(req.params.promotionId, function(err, resp) {
+      if (err)  throw err;
+      res.json(resp);
+    });
   });
 
-module.exports = promoRouter;
+module.exports = promotionRouter;
