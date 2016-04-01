ae401d3ecc77173a3ebdea3568820346310096a9
Anson <anson32330200@gmail.com>
2016-04-01 14:18:06 +0800

    Add in code to connect to the MongoDB server

--- rest-server/app.js
+++ rest-server/app.js
@@ -5,6 +5,17 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Connect to MongoDB server
+var mongoose = require('mongoose');
+var url = 'mongodb://localhost:27017/conFusion';
+mongoose.connect(url);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+    console.log("Connected correctly to server");
+});
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
