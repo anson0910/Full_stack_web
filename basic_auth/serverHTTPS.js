43617e6554d803a6319badfbd543f6040316161c
Anson <anson32330200@gmail.com>
2016-04-02 17:46:17 +0800

    Perform basic authentication through http/https with username and password in a browser

---
+++ basic_auth/serverHTTPS.js
@@ -0,0 +1,66 @@
+// Server that servers ports at HTTP and HTTPS
+var express = require('express');
+// Middleware
+var morgan = require('morgan');
+
+var hostname = 'localhost';
+
+var http = require('http');
+var https = require('https');
+var fs = require("fs");
+var port = 3000;
+var httpsPort = 8443;
+// Setup HTTPS
+var options = {
+  key: fs.readFileSync('./private.key'),
+  cert: fs.readFileSync('./certificate.pem')
+};
+
+var app = express();
+
+app.use(morgan('dev'));
+
+// Basic authentication function
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader)  {
+    var err = new Error("You are not authenticated!");
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == "user" && pass == "password") {
+    next(); // authorized
+  }
+  else {
+    var err = new Error("You are not authenticated!");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
+// Catch error
+app.use(function(err, req, res, next) {
+  res.writeHead(err.status || 500, {
+    'WWW-Authenticate': 'Basic',
+    'Content-Type': 'text/plain'
+  });
+  res.end(err.message);
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+https.createServer(options, app).listen(httpsPort, hostname, function () {
+    console.log(`Server running at https://${hostname}:${httpsPort}/`);
+});
