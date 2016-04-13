743bc923537f1c7bd61c7272b233c2bea77e1660
Anson <anson32330200@gmail.com>
2016-04-13 20:17:48 +0800

    Set up a MongoDB as a data source and then set up access controls on the REST API endpoints

---
+++ loopback-server/server/boot/script.js
@@ -0,0 +1,33 @@
+module.exports = function(app)  {
+  var MongoDB = app.dataSources.MongoDB;
+
+  MongoDB.automigrate('Customer', function(err) {
+    if (err) throw(err);
+    var Customer = app.models.Customer;
+
+    Customer.create([
+        {username: 'Admin', email: 'admin@admin.com', password: 'password'},
+        {username: 'anson', email: 'anson@ntu.tw', password: 'password'}
+      ],
+      function(err, users) {
+        if (err) throw(err);
+        var Role = app.models.Role;
+        var RoleMapping = app.models.RoleMapping;
+
+        //create the admin role
+        Role.create({
+            name: 'admin'
+          },
+          function(err, role) {
+            if (err) cb(err);
+             //make admin
+            role.principals.create({
+              principalType: RoleMapping.USER,
+              principalId: users[0].id
+            }, function(err, principal) {
+                if (err) throw(err);
+            });
+        });
+      });
+  });
+};
